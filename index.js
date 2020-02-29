a5f67aca31190712b6bae1ca383cbb83c37d3e93
felipe <felipe.negri43@gmail.com>
2020-02-29 18:31:18 -0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Sound = require('node-aplay');
- 
-// fire and forget:
-new Sound('/sounds/nut.mp3').play();
- 
+const player = require('play-sound')();
+
+player.play('./sounds/nut.mp3', (err) => {
+    if (err) console.log(`Could not play sound: ${err}`);
+});
